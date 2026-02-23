836ad6116e956239da6c71d363cc72a406f88001
zhuPian <3587746736@qq.com>
2026-02-23 19:55:27 +0800

    初始化后端项目
    1. 初始化项目
    2. 添加依赖和配置（HuTool 和 Knife4j 接口文档）
    3. 提供基础代码（自定义异常、请求响应包装类）
    4. 提供基础接口（健康检查接口）

---
+++ src/main/java/com/yupi/yuaicodemother/YuAiCodeMotherApplication.java
@@ -0,0 +1,13 @@
+package com.yupi.yuaicodemother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuAiCodeMotherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuAiCodeMotherApplication.class, args);
+    }
+
+}
