33a53afc7d5ba235dc34c47ccf9c3902b355fb2a
zhuPian <3587746736@qq.com>
2026-03-03 01:02:42 +0800

    写了用户后端
    1. 开发 MyBatis Flex 代码生成器
    2. 开发核心功能，比如用户注册、用户登录、获取登录用户信息、用户注销等
    3. 全局权限校验 AOP
    4. 开发管理员增删改查接口
    5. 自定义配置解决丢失精度问题

--- src/main/java/com/yupi/yuaicodemother/YuAiCodeMotherApplication.java
+++ src/main/java/com/yupi/yuaicodemother/YuAiCodeMotherApplication.java
@@ -1,9 +1,11 @@
 package com.yupi.yuaicodemother;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.yupi.yuaicodemother.mapper")
 public class YuAiCodeMotherApplication {
 
     public static void main(String[] args) {
