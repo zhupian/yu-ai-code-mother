f421d296db68541a6af37bb4031ba8d31ecfddf3
zhuPian <3587746736@qq.com>
2026-03-07 08:02:03 +0800

    先把ai生成跑起来
    1. 接入 LangChain4j，实现 AI 对话服务
    2. 开发文件保存器和代码解析器、AI 生成门面，实现 AI 生成网站文件
    3. SSE 流式输出和解析

---
+++ src/main/java/com/yupi/yuaicodemother/ai/AiCodeGeneratorServiceFactory.java
@@ -0,0 +1,34 @@
+package com.yupi.yuaicodemother.ai;
+
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.StreamingChatModel;
+import dev.langchain4j.service.AiServices;
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * AI 服务创建工厂
+ */
+@Configuration
+public class AiCodeGeneratorServiceFactory {
+
+    @Resource
+    private ChatModel chatModel;
+
+    @Resource
+    private StreamingChatModel streamingChatModel;
+
+    /**
+     * 创建 AI 代码生成器服务
+     *
+     * @return
+     */
+    @Bean
+    public AiCodeGeneratorService aiCodeGeneratorService() {
+        return AiServices.builder(AiCodeGeneratorService.class)
+                .chatModel(chatModel)
+                .streamingChatModel(streamingChatModel)
+                .build();
+    }
+}
---
+++ src/test/java/com/yupi/yuaicodemother/core/AiCodeGeneratorFacadeTest.java
@@ -0,0 +1,36 @@
+package com.yupi.yuaicodemother.core;
+
+import com.yupi.yuaicodemother.model.enums.CodeGenTypeEnum;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
+
+import java.io.File;
+import java.util.List;
+
+@SpringBootTest
+class AiCodeGeneratorFacadeTest {
+
+    @Resource
+    private AiCodeGeneratorFacade aiCodeGeneratorFacade;
+
+    @Test
+    void generateAndSaveCode() {
+        File file = aiCodeGeneratorFacade.generateAndSaveCode("生成一个登录页面，总共不超过 20 行代码", CodeGenTypeEnum.MULTI_FILE);
+        Assertions.assertNotNull(file);
+    }
+
+    @Test
+    void generateAndSaveCodeStream() {
+        Flux<String> codeStream = aiCodeGeneratorFacade.generateAndSaveCodeStream("生成一个登录页面，总共不超过 20 行代码", CodeGenTypeEnum.MULTI_FILE);
+        // 阻塞等待所有数据收集完成
+        List<String> result = codeStream.collectList().block();
+        // 验证结果
+        Assertions.assertNotNull(result);
+        // 拼接字符串，得到完整内容
+        String completeContent = String.join("", result);
+        Assertions.assertNotNull(completeContent);
+    }
+}
