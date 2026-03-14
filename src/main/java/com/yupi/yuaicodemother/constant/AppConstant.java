30672ecab6576b700f81bccabfdc5990502d7666
zhuPian <3587746736@qq.com>
2026-03-14 13:09:18 +0800

    完成应用基本功能
    1. AI 生成基础功能代码
    2. 完成基本功能、人工测试通过

---
+++ src/main/java/com/yupi/yuaicodemother/constant/AppConstant.java
@@ -0,0 +1,17 @@
+package com.yupi.yuaicodemother.constant;
+
+/**
+ * 应用常量
+ */
+public interface AppConstant {
+
+    /**
+     * 精选应用的优先级
+     */
+    Integer GOOD_APP_PRIORITY = 99;
+
+    /**
+     * 默认应用优先级
+     */
+    Integer DEFAULT_APP_PRIORITY = 0;
+}
--- src/main/java/com/yupi/yuaicodemother/service/AppService.java
+++ src/main/java/com/yupi/yuaicodemother/service/AppService.java
@@ -1,7 +1,12 @@
 package com.yupi.yuaicodemother.service;
 
+import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
+import com.yupi.yuaicodemother.model.dto.app.AppQueryRequest;
 import com.yupi.yuaicodemother.model.entity.App;
+import com.yupi.yuaicodemother.model.vo.AppVO;
+
+import java.util.List;
 
 /**
  * 应用 服务层。
@@ -9,5 +14,29 @@ import com.yupi.yuaicodemother.model.entity.App;
  * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
  */
 public interface AppService extends IService<App> {
+
+    /**
+     * 获取应用封装类
+     *
+     * @param app
+     * @return
+     */
+    AppVO getAppVO(App app);
+
+    /**
+     * 获取应用封装类列表
+     *
+     * @param appList
+     * @return
+     */
+    List<AppVO> getAppVOList(List<App> appList);
+
+    /**
+     * 构造应用查询条件
+     *
+     * @param appQueryRequest
+     * @return
+     */
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
 
 }
