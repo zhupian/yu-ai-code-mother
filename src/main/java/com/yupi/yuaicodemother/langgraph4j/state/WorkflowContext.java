a39c5e32f855f070aa94c8da2667c13a25cfc495
zhuPian <3587746736@qq.com>
2026-04-16 14:51:39 +0800

    调整图片收集逻辑
    1. 基于 LangGraph4j Parallel Branch 实现并发

--- src/main/java/com/yupi/yuaicodemother/langgraph4j/state/WorkflowContext.java
+++ src/main/java/com/yupi/yuaicodemother/langgraph4j/state/WorkflowContext.java
@@ -1,5 +1,6 @@
 package com.yupi.yuaicodemother.langgraph4j.state;
 
+import com.yupi.yuaicodemother.langgraph4j.model.ImageCollectionPlan;
 import com.yupi.yuaicodemother.langgraph4j.model.ImageResource;
 import com.yupi.yuaicodemother.langgraph4j.model.QualityResult;
 import com.yupi.yuaicodemother.model.enums.CodeGenTypeEnum;
@@ -77,6 +78,19 @@ public class WorkflowContext implements Serializable {
      * 错误信息
      */
     private String errorMessage;
+
+    /**
+     * 图片收集计划
+     */
+    private ImageCollectionPlan imageCollectionPlan;
+
+    /**
+     * 并发图片收集的中间结果字段
+     */
+    private List<ImageResource> contentImages;
+    private List<ImageResource> illustrations;
+    private List<ImageResource> diagrams;
+    private List<ImageResource> logos;
 
     @Serial
     private static final long serialVersionUID = 1L;
